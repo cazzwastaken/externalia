55b43118e995342be76ab45b4bfd933f076ea8a5
cazzwastaken <cazzwastaken@gmail.com>
2021-12-29 20:05:35 +0200

    Added run bool for later

--- cheat/globals.h
+++ cheat/globals.h
@@ -14,6 +14,8 @@ namespace o {
 
 // globals
 namespace g {
+	inline bool run = true;
+
 	inline std::vector<c_entity> entity_list;
 	inline c_entity local_player;
 
@@ -23,5 +25,9 @@ namespace g {
 
 	constexpr const std::int32_t max_players() noexcept {
 		return m::read< std::int32_t>(client_state() + 0x388);
+	}
+
+	constexpr const std::uintptr_t glow_object_manager() noexcept {
+		return m::read<std::uintptr_t>(m::client + o::dw_glow_object_manager);
 	}
 }
