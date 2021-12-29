023d368a5ffd359f78efb63c8b81172bcc9388dc
cazzwastaken <cazzwastaken@gmail.com>
2021-12-29 18:47:33 +0200

    Added entity list population loop + a test hack feature (radar)

--- cheat/main.cpp
+++ cheat/main.cpp
@@ -1,11 +1,12 @@
 #include <iostream>
 #include <format>
 #include <thread>
+#include <algorithm>
 
 #define WIN32_LEAN_AND_MEAN
 #include <Windows.h>
 
-#include "memory.h"
+#include "globals.h"
 
 void print(const std::string& text) noexcept {
 	std::cout << text;
@@ -51,9 +52,52 @@ int main() {
 		return 1;
 	}
 
-	// gui loop
-	while (true) {
+	g::entity_list.reserve(32);
 
+	while (true) {
+		std::this_thread::sleep_for(std::chrono::milliseconds(5));
+
+		g::local_player.set_address(m::client + o::dw_local_player);
+
+		// not in game
+		if (g::max_players() == 0) {
+			g::entity_list.clear();
+			continue;
+		}
+
+		// populate entity list
+		for (std::uintptr_t i = 1; i <= 32; ++i) {
+			const auto entity = c_entity{ m::client + o::dw_entity_list + i * 0x10 };
+
+			if (!entity.is_valid()) {
+				continue;
+			}
+
+			if (entity == g::local_player) {
+				continue;
+			}
+
+			const auto result = std::find_if(
+				g::entity_list.begin(),
+				g::entity_list.end(),
+				[&entity](const c_entity& ent) noexcept -> bool { return ent == entity; }
+			);
+
+			// it exists in the list already
+			if (result != std::end(g::entity_list)) {
+				continue;
+			}
+
+			g::entity_list.emplace_back(entity);
+		}
+
+		// this temporay, for testing the entity list vector
+		for (const auto& entity : g::entity_list) {
+			if (entity.get_team() == g::local_player.get_team())
+				continue;
+
+			entity.set_spotted(true);
+		}
 	}
 
 	// free handle resources
