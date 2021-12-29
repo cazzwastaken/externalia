3d721dfde583b350103b27f4dd1c27b3d0869b5c
cazzwastaken <cazzwastaken@gmail.com>
2021-12-29 18:47:05 +0200

    Updated offsets & added max_players fn

--- cheat/globals.h
+++ cheat/globals.h
@@ -1,14 +1,27 @@
 #pragma once
 #include <vector>
 
+#include "sdk/entity.h"
+
+// offsets
 namespace o {
 	constexpr std::ptrdiff_t dw_client_state = 0x589FC4;
-	constexpr std::ptrdiff_t dw_entity_list = 0x4DCFA94;
-	constexpr std::ptrdiff_t dw_force_jump = 0x527998C;
-	constexpr std::ptrdiff_t dw_glow_object_manager = 0x5317E28;
-	constexpr std::ptrdiff_t dw_local_player = 0xDB458C;
+	constexpr std::ptrdiff_t dw_entity_list = 0x4DD0AB4;
+	constexpr std::ptrdiff_t dw_force_jump = 0x527A9AC;
+	constexpr std::ptrdiff_t dw_glow_object_manager = 0x5318E50;
+	constexpr std::ptrdiff_t dw_local_player = 0xDB558C;
 }
 
+// globals
 namespace g {
-	inline std::vector<class c_entity> entity_list;
+	inline std::vector<c_entity> entity_list;
+	inline c_entity local_player;
+
+	constexpr const std::uintptr_t client_state() noexcept {
+		return m::read<std::uintptr_t>(m::engine + o::dw_client_state);
+	}
+
+	constexpr const std::int32_t max_players() noexcept {
+		return m::read< std::int32_t>(client_state() + 0x388);
+	}
 }
