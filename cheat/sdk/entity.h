b605341756c6d461cc506c0f3dc078a70f8d8967
cazzwastaken <cazzwastaken@gmail.com>
2021-12-29 20:05:19 +0200

    Dormancy check, set render & removed color_t

--- cheat/sdk/entity.h
+++ cheat/sdk/entity.h
@@ -1,9 +1,7 @@
 #pragma once
 #include "../memory.h"
 
-struct color_t {
-	std::uint8_t r, g, b;
-};
+#include "color.h"
 
 class c_entity {
 	constexpr const std::uintptr_t team() const noexcept { return 0xF4; }
@@ -12,6 +10,7 @@ class c_entity {
 	constexpr const std::uintptr_t flags() const noexcept { return 0x104; }
 	constexpr const std::uintptr_t render() const noexcept { return 0x70; }
 	constexpr const std::uintptr_t glow_index() const noexcept { return 0x10488; }
+	constexpr const std::uintptr_t dormant() const noexcept { return 0xED; }
 
 public:
 	c_entity() = default;
@@ -41,6 +40,10 @@ public:
 		return m::read<int>(_addr + glow_index());
 	}
 
+	constexpr const bool is_dormant() const noexcept {
+		return m::read<bool>(_addr + dormant());
+	}
+
 	constexpr const bool is_alive() const noexcept {
 		return m::read<int>(_addr + life_state()) == 0;
 	}
@@ -49,7 +52,7 @@ public:
 		m::write(_addr + spotted(), is_spotted);
 	}
 
-	constexpr void set_render(const color_t& clr) const noexcept {
+	constexpr void set_render(const color3_t<std::uint8_t>& clr) const noexcept {
 		m::write(_addr + render(), clr);
 	}
 
